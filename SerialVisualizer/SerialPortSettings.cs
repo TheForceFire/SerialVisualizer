ee47fdbde171b77a8cde600ee98f7c53d59ba0a9
MiXsssp <xdmixme@gmail.com>
2026-04-04 12:45:10 +0400

    добавил код

--- SerialVisualizer/SerialPortSettings.cs
+++ SerialVisualizer/SerialPortSettings.cs
@@ -1,9 +1,11 @@
-﻿using System.IO.Ports;
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
 using System.Text.Json.Serialization;
 
 namespace SerialVisualizer
 {
-    //
     internal class SerialPortSettings
     {
         [JsonInclude]
@@ -25,40 +27,52 @@ namespace SerialVisualizer
             this.StopBits = StopBits;
         }
 
-        public int getBaudRate()
+
+        public static byte[] GenerateCommand(byte[] header, byte[] address, byte[] payload, bool useCrc)
         {
-            return BaudRate;
-        }
-        public void setBaudRate(int value)
-        {
-            BaudRate = value;
+            List<byte> packet = new List<byte>();
+
+
+            if (header != null) packet.AddRange(header);
+            if (address != null) packet.AddRange(address);
+            if (payload != null) packet.AddRange(payload);
+
+
+            if (useCrc)
+            {
+                byte crc = CalculateCRC8(packet.ToArray());
+                packet.Add(crc);
+            }
+
+            return packet.ToArray();
         }
 
-        public Parity getParity()
+        private static byte CalculateCRC8(byte[] data)
         {
-            return Parity;
+            byte crc = 0xFF;
+            for (int i = 0; i < data.Length; i++)
+            {
+                crc ^= data[i];
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((crc & 0x80) != 0)
+                        crc = (byte)((crc << 1) ^ 0x31);
+                    else
+                        crc <<= 1;
+                }
+            }
+            return crc;
         }
-        public void setParity(Parity value)
-        {
-            Parity = value;
-        }
+        public int getBaudRate() => BaudRate;
+        public void setBaudRate(int value) => BaudRate = value;
 
-        public int getDataBits()
-        {
-            return DataBits;
-        }
-        public void setDataBits(int value)
-        {
-            DataBits = value;
-        }
+        public Parity getParity() => Parity;
+        public void setParity(Parity value) => Parity = value;
 
-        public StopBits getStopBits()
-        {
-            return StopBits;
-        }
-        public void setStopBits(StopBits value)
-        {
-            StopBits = value;
-        }
+        public int getDataBits() => DataBits;
+        public void setDataBits(int value) => DataBits = value;
+
+        public StopBits getStopBits() => StopBits;
+        public void setStopBits(StopBits value) => StopBits = value;
     }
 }
