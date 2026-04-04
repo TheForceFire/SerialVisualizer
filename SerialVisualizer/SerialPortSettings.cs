60a5103396410694ad7269f9eeb3d3314ec2244a
MiXsssp <xdmixme@gmail.com>
2026-04-04 12:54:42 +0400

    добавил новый класс

--- SerialVisualizer/SerialPortSettings.cs
+++ SerialVisualizer/SerialPortSettings.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO.Ports;
-using System.Linq;
+﻿using System.IO.Ports;
 using System.Text.Json.Serialization;
 
 namespace SerialVisualizer
@@ -9,13 +6,13 @@ namespace SerialVisualizer
     internal class SerialPortSettings
     {
         [JsonInclude]
-        private int BaudRate;
+        public int BaudRate;
         [JsonInclude]
-        private Parity Parity;
+        public Parity Parity;
         [JsonInclude]
-        private int DataBits;
+        public int DataBits;
         [JsonInclude]
-        private StopBits StopBits;
+        public StopBits StopBits;
 
         public SerialPortSettings() { }
 
@@ -27,52 +24,16 @@ namespace SerialVisualizer
             this.StopBits = StopBits;
         }
 
+        public int getBaudRate() { return BaudRate; }
+        public void setBaudRate(int value) { BaudRate = value; }
 
-        public static byte[] GenerateCommand(byte[] header, byte[] address, byte[] payload, bool useCrc)
-        {
-            List<byte> packet = new List<byte>();
+        public Parity getParity() { return Parity; }
+        public void setParity(Parity value) { Parity = value; }
 
+        public int getDataBits() { return DataBits; }
+        public void setDataBits(int value) { DataBits = value; }
 
-            if (header != null) packet.AddRange(header);
-            if (address != null) packet.AddRange(address);
-            if (payload != null) packet.AddRange(payload);
-
-
-            if (useCrc)
-            {
-                byte crc = CalculateCRC8(packet.ToArray());
-                packet.Add(crc);
-            }
-
-            return packet.ToArray();
-        }
-
-        private static byte CalculateCRC8(byte[] data)
-        {
-            byte crc = 0xFF;
-            for (int i = 0; i < data.Length; i++)
-            {
-                crc ^= data[i];
-                for (int j = 0; j < 8; j++)
-                {
-                    if ((crc & 0x80) != 0)
-                        crc = (byte)((crc << 1) ^ 0x31);
-                    else
-                        crc <<= 1;
-                }
-            }
-            return crc;
-        }
-        public int getBaudRate() => BaudRate;
-        public void setBaudRate(int value) => BaudRate = value;
-
-        public Parity getParity() => Parity;
-        public void setParity(Parity value) => Parity = value;
-
-        public int getDataBits() => DataBits;
-        public void setDataBits(int value) => DataBits = value;
-
-        public StopBits getStopBits() => StopBits;
-        public void setStopBits(StopBits value) => StopBits = value;
+        public StopBits getStopBits() { return StopBits; }
+        public void setStopBits(StopBits value) { StopBits = value; }
     }
 }
